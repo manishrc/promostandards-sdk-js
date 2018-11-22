d290bb16405c5e0b2e9566949f4358c7029430e8
Manish Chiniwalar <hi@manishrc.com>
2018-11-22 22:17:56 +0530

    Add ProductData service methods
    * Use Pug to render XML because node-soap had issues with PS endpoints
    * Add basic documentations using JSDoc

---
+++ lib/PromoStandards.ts
@@ -0,0 +1,162 @@
+'use strict';
+const pug = require('pug');
+const axios = require('axios');
+import * as templates from './templates';
+import { declareInterface } from 'babel-types';
+
+export namespace PromoStandards {
+  interface PromoStandardsAPICallParams {}
+  interface Method<MethodArguments extends PromoStandardsAPICallParams> {
+    (params: MethodArguments): Promise<any>;
+  }
+
+  /** Base Attributes for a PromoStandards Client */
+  interface PromoStandardsBaseAttributes {
+    id?: string;
+    password?: string;
+    endpoints?: ServiceEndpointType[];
+  }
+
+  /** Type of service check */
+  type ServiceType =
+    | 'Inventory'
+    | 'Invoice'
+    | 'MediaContent'
+    | 'OrderShipmentNotification'
+    | 'OrderStatus'
+    | 'ProductData'
+    | 'ProductPricingAndConfiguration'
+    | 'PurchaseOrder';
+
+  /** Service endpoint object signature check */
+  type ServiceEndpointType = {
+    type: ServiceType;
+    version: string; // @todo create a SemVer type
+    url: string; // @todo create a valid URL type
+  };
+
+  /** PromoStandards method name check */
+  type MethodType =
+    | 'getFilterValues'
+    | 'getInventoryLevels'
+    | 'getMediaContent'
+    | 'getMediaDateModified'
+    | 'getOrderShipmentNotification'
+    | 'getOrderStatusDetails'
+    | 'getOrderStatusTypes'
+    | 'getProduct'
+    | 'getProductDateModified'
+    | 'getProductSellable'
+    | 'getProductCloseOut'
+    | 'getAvailableLocations'
+    | 'getDecorationColors'
+    | 'getFobPoints'
+    | 'getAvailableCharges'
+    | 'GetConfigurationAndPricing'
+    | 'getSupportedOrderTypes'
+    | 'sendPO';
+
+  type ProductDataGetProductArguments = PromoStandardsBaseAttributes & {
+    productId: string;
+    localizationCountry: string;
+    localizationLanguage: string;
+    partId?: string;
+    colorName?: string;
+    ApparelSizeArray?: any[];
+  };
+
+  /** Class representing a PromoStandards Client */
+  export class Client {
+    public id?: string;
+    public password?: string;
+    public endpoints?: ServiceEndpointType[];
+
+    /**
+     * Create a new PromoStandards Client
+     * @param {string} options.id - Username provided by the supplier
+     * @param {string} options.password - Password provided by the supplier
+     * @param {Array.<ServiceEndpointType>} options.endpoints - List of endpoint objects
+     */
+    constructor(options: PromoStandardsBaseAttributes = {}) {
+      this.id = options.id;
+      this.password = options.password;
+      this.endpoints = options.endpoints;
+    }
+
+    /**
+     * Get the service endpoint, if present.
+     * @param {ServiceName} serviceName Service Endpoint Name
+     */
+    public getEndpoint(serviceName: ServiceType): ServiceEndpointType {
+      let endpoint;
+      if (this.endpoints && this.endpoints.length > 0) {
+        endpoint = this.endpoints.find(
+          x => x.type === serviceName,
+        ) as ServiceEndpointType;
+      }
+      if (endpoint) return endpoint;
+      throw new ReferenceError(`'${serviceName}' endpoint is undefined`);
+    }
+
+    /**
+     * Generic method to use for all PS methods
+     * @param {string} methodName - Identifies the PromoStandards service time and method name
+     * @param params - Arguments required for the given PromoStandards method
+     * @todo validate arguments based on service/method
+     * @todo pick template based on service/method
+     * @todo render request using provided options
+     * @todo make request
+     * @todo return response
+     * */
+    public promoStandardsAPIRequest(
+      methodName: string,
+      params: any,
+    ): Promise<any> {
+      return new Promise((resolve, reject) => {
+        const [service, method] = methodName.split('.');
+        const endpoint = this.getEndpoint(service as ServiceType);
+
+        /** @todo fix type check*/
+        const templateIndex: {
+          [index: string]: any;
+        } = templates;
+
+        const requestXML: string = templateIndex[method](
+          Object.assign(
+            {
+              id: this.id,
+              password: this.password,
+              wsVersion: endpoint.version,
+            },
+            params,
+          ),
+        );
+        axios
+          .post(endpoint.url, requestXML, {
+            headers: { 'Content-Type': 'text/xml' },
+          })
+          .then((result: any) => resolve(result.data));
+      });
+    }
+
+    /** @todo Add TypeChecking for all methods. */
+    public readonly productData = {
+      getProduct: this.promoStandardsAPIRequest.bind(
+        this,
+        'ProductData.getProduct',
+      ),
+      getProductSellable: this.promoStandardsAPIRequest.bind(
+        this,
+        'ProductData.getProductSellable',
+      ),
+      getProductDateModified: this.promoStandardsAPIRequest.bind(
+        this,
+        'ProductData.getProductDateModified',
+      ),
+      getProductCloseOut: this.promoStandardsAPIRequest.bind(
+        this,
+        'ProductData.getProductCloseOut',
+      ),
+    };
+  }
+}
---
+++ lib/templates.ts
@@ -0,0 +1,81 @@
+const pug = require('pug');
+
+/** @todo Add support for ApparelSizeArray */
+export const getProduct: () => string = pug.compile(
+  `soapenv:Envelope(
+    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:ns="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/"
+    xmlns:shar="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/"
+  )
+  soapenv:Header/
+  soapenv:Body
+    ns:GetProductRequest
+      shar:wsVersion #{wsVersion}
+      shar:id #{id}
+      if password
+        shar:password #{password}
+      shar:localizationCountry #{localizationCountry}
+      shar:localizationLanguage #{localizationLanguage}
+      shar:productId #{productId}
+      if partId
+        shar:partId #{partId}
+      if colorName
+        shar:colorName #{colorName}
+  `,
+);
+
+export const getProductSellable: () => string = pug.compile(
+  `soapenv:Envelope(
+    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:ns="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/"
+    xmlns:shar="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/"
+  )
+  soapenv:Header/
+  soapenv:Body
+    ns:GetProductSellableRequest
+      shar:wsVersion #{wsVersion}
+      shar:id #{id}
+      if password
+        shar:password #{password}
+      shar:productId #{productId}
+      if partId
+        shar:partId #{partId}
+      shar:isSellable #{isSellable}
+  `,
+);
+
+export const getProductDateModified: () => string = pug.compile(
+  `soapenv:Envelope(
+    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:ns="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/"
+    xmlns:shar="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/"
+  )
+  soapenv:Header/
+  soapenv:Body
+    ns:GetProductDateModifiedRequest
+      shar:wsVersion #{wsVersion}
+      shar:id #{id}
+      if password
+        shar:password #{password}
+      shar:productId #{productId}
+      if partId
+        shar:partId #{partId}
+      shar:changeTimeStamp #{changeTimeStamp}
+  `,
+);
+
+export const getProductCloseOut: () => string = pug.compile(
+  `soapenv:Envelope(
+    xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+    xmlns:ns="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/"
+    xmlns:shar="http://www.promostandards.org/WSDL/ProductDataService/1.0.0/SharedObjects/"
+  )
+  soapenv:Header/
+  soapenv:Body
+    ns:GetProductCloseOutRequest
+      shar:wsVersion #{wsVersion}
+      shar:id #{id}
+      if password
+        shar:password #{password}
+  `,
+);
